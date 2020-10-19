2cd303d3be4997c68281426b7391b7fdec68a157
Arwildo <arwildo@gmail.com>
2020-10-19 21:14:33 +0900

    add tailwind styles

--- src/components/tools.js
+++ src/components/tools.js
@@ -58,36 +58,79 @@ class Tools extends React.Component {
 
 	render() {
 		return (
-			<div className="row">
-				<div className="col-md-4 col-md-offset-2">
-					<br />
-					<textarea
-						type="text"
-						placeholder="Input ... "
-						ref={(ref) => (this.nameTextInput = ref)}
-						className="form-control"
-					/>
-				</div>
-				<div className="col-md-4">
-					<br />
-					<button type="button" className="btn btn-success" onClick={this.allCaps}>
-						t-T
-					</button>
-					<button type="button" className="btn btn-success" onClick={this.allLows}>
-						T-t
-					</button>
-					<button type="button" className="btn btn-success" onClick={this.upperFirst}>
-						abc-Abc
-					</button>
-					<button type="button" className="btn btn-success" onClick={this.reverseText}>
-						abc-cba
-					</button>
-				</div>
-				<br />
-
-				<div className="row">
-					<div className="col-md-4 col-md-offset-2">
-						<textarea type="text" placeholder="Output ... " value={this.state.text} />
+			<div class="bg-gray-100">
+				<div class="max-w-7xl mx-auto py-12 sm:px-6 lg:px-8">
+					<div class="max-w-4xl mx-auto">
+						<div class="bg-white shadow overflow-hidden sm:rounded-lg">
+							<div class="px-4 py-5 border-b border-gray-200 sm:px-6">
+								<h3 class="text-lg leading-6 font-medium text-gray-900">Logo</h3>
+								<p class="mt-1 max-w-2xl text-sm leading-5 text-gray-500">Simple tools for your text</p>
+								<div class="pl-3 pr-4 py-3 justify-center text-sm leading-5">
+									<textarea
+										type="text"
+										placeholder="Input ... "
+										ref={(ref) => (this.nameTextInput = ref)}
+										className="transition-colors duration-100 ease-in-out text-gray-600 py-2 pr-6 pl-8 block w-full h-32 appearance-none leading-normal border border-transparent rounded-lg focus:outline-none text-left select-none truncate focus:bg-white focus:border-gray-200 bg-gray-200"
+									/>
+									<br />
+									<div className="flex justify-center">
+										<br />
+										<button
+											type="button"
+											className="m-1 bg-white hover:bg-gray-100 text-gray-800 font-semibold py-2 px-4 border border-gray-400 rounded shadow"
+											onClick={this.allCaps}
+										>
+											t-T
+										</button>
+										<button
+											type="button"
+											className="m-1 bg-white hover:bg-gray-100 text-gray-800 font-semibold py-2 px-4 border border-gray-400 rounded shadow"
+											onClick={this.allLows}
+										>
+											T-t
+										</button>
+										<button
+											type="button"
+											className="m-1 bg-white hover:bg-gray-100 text-gray-800 font-semibold py-2 px-4 border border-gray-400 rounded shadow"
+											onClick={this.upperFirst}
+										>
+											abc-Abc
+										</button>
+										<button
+											type="button"
+											onClick={this.reverseText}
+											className="m-1 bg-white hover:bg-gray-100 text-gray-800 font-semibold py-2 px-4 border border-gray-400 rounded shadow"
+										>
+											abc-cba
+										</button>
+									</div>
+									<br />
+									<textarea
+										type="text"
+										placeholder="Output ... "
+										value={this.state.text}
+										className="transition-colors duration-100 ease-in-out text-gray-600 py-2 pr-6 pl-8 block w-full h-32 appearance-none leading-normal border border-transparent rounded-lg focus:outline-none text-left select-none truncate focus:bg-white focus:border-gray-200 bg-gray-200"
+									/>
+								</div>
+								<div className="flex justify-center">
+									<br />
+									<button
+										type="button"
+										className="m-1 bg-white hover:bg-gray-100 text-gray-800 font-semibold py-2 px-4 border border-gray-400 rounded shadow"
+										onClick={this.allCaps}
+									>
+										Copy
+									</button>
+									<button
+										type="button"
+										className="m-1 bg-white hover:bg-red-100 text-gray-800 font-semibold py-2 px-4 border border-gray-400 rounded shadow"
+										onClick={this.allLows}
+									>
+										Clear
+									</button>
+								</div>
+							</div>
+						</div>
 					</div>
 				</div>
 			</div>
