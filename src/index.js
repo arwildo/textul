3443f34c68aef190678e918862aaafcd3e8fa200
Arwildo <arwildo@gmail.com>
2020-10-22 17:10:06 +0900

    add google analytics

--- src/index.js
+++ src/index.js
@@ -4,10 +4,13 @@ import './styles.css';
 import Tools from './components/tools';
 import Features from './components/features';
 import Footer from './components/footer';
+import ReactGA from 'react-ga';
 import './components/assets/img/logo.png';
 
 const Index = () => {
 	return [ <Tools />, <Features />, <Footer /> ];
 };
 
+ReactGA.initialize('UA-51659765-2');
+ReactGA.pageview(window.location.pathname + window.location.search);
 ReactDOM.render(<Index />, document.getElementById('index'));
